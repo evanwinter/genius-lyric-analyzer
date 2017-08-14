23136cc33dad028922967ec2771ee05a087f96bc
Evan Winter <ewinter1996@gmail.com>
2017-08-13 23:00:34 -0500

    .

--- analyze-lyrics.py
+++ analyze-lyrics.py
@@ -3,6 +3,7 @@ import os
 import re
 import requests
 import json
+import string
 from bs4 import BeautifulSoup
 import config
 from collections import Counter
@@ -11,18 +12,17 @@ api = "https://api.genius.com"
 client_access_token = config.client_access_token
 headers = {'Authorization': 'Bearer ' + client_access_token}
 
-
-def setup(artist_name, song_title):
+def setup(artist_name):
 	reload(sys)
 	sys.setdefaultencoding('utf8')
 	if not os.path.exists("output/"):
 		os.makedirs("output/")
-	lyrics_output = "output/" + re.sub(r"[^A-Za-z]+", '', artist_name) + "-" + re.sub(r"[^A-Za-z]+", '', song_title) + ".txt"
+	lyrics_output = "output/" + re.sub(r"[^A-Za-z]+", '', artist_name) + ".txt"
 	return lyrics_output
 
-def get_song(artist_name, song_title, lyrics_output):
+def get_songs(artist_name, lyrics_output):
 	search_url = api + "/search"
-	data = {'q': song_title + ' ' + artist_name}
+	data = {'q': artist_name}
 	response = requests.get(search_url, data=data, headers=headers)
 	response_json = response.json()
 
@@ -31,26 +31,22 @@ def get_song(artist_name, song_title, lyrics_output):
 	
 	hits = response_json["response"]["hits"]
 	num_hits = len(hits)
-	print(json.dumps(hits, indent=4, sort_keys=True))
 	if num_hits == 0:
-		print("No results for: " + song_title + " by " + artist_name + ".")
-
-	for hit in hits:
-		if hit["result"]["primary_artist"]["name"].lower() == artist_name.lower():
-			print("Match found.")
-			song_info = hit
-			break
+		print("No results for: " + artist_name + ".")
 
 	lyrics = ''
+	all_lyrics = ''
 
-	if song_info:
+	for hit in hits:
+		song_info = hit
 		song_api_path = song_info["result"]["api_path"]
 		lyrics = get_lyrics(song_api_path).lower()
 		with open(lyrics_output, 'a') as f:
 			f.write(lyrics)
-			f.write(str(Counter(lyrics.split()).most_common()))
-	else:
-		print("No match found.")
+			all_lyrics += lyrics
+
+	with open(lyrics_output, 'a') as f:
+		f.write(str(Counter(all_lyrics.split()).most_common()))
 
 def get_lyrics(song_api_path):
 	song_url = api + song_api_path
@@ -65,18 +61,16 @@ def get_lyrics(song_api_path):
 	[h.extract() for h in html('script')]
 
 	lyrics = html.find("div", class_="lyrics").get_text()
-	print lyrics
 	return lyrics
 
 def main():
 	arguments = sys.argv[1:]
-	song_title = arguments[0].translate(None, "\'\"")
-	artist_name = arguments[1].translate(None, "\'\"")
-	lyrics_output = setup(artist_name, song_title)
+	artist_name = arguments[0].translate(None, "\'\"")
+	lyrics_output = setup(artist_name)
 
-	print('Looking for ' + song_title + ' by ' + artist_name + '...')
+	print('Looking for songs by ' + artist_name + '...')
 
-	get_song(artist_name, song_title, lyrics_output)
+	get_songs(artist_name, lyrics_output)
 
 if __name__ == '__main__':
 	main()
